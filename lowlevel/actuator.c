d4c58408676c819f63b880e9cee6810c7f72fe43
Victor <victor.millory@outlook.fr>
2020-07-10 12:06:45 +0200

    indentation 3

--- lowlevel/actuator.c
+++ lowlevel/actuator.c
@@ -64,48 +64,48 @@ void actuator_setup(){
 void actuator_set(enum actuator_sel sel, int8_t value){
     /*value should be between -100 and +100*/
     if(value < -100 || value > 100){
-    	return;
+        return;
     }
 
     int speed;
     switch(sel){
-    	case ARM:
-    		/* redefine forward direction */
-    		value *= ARM_INVERT_DIR;
-    	
-    		/*direction is backward: set DIR to 1 and value become positive*/
-    		if(value<0){
+        case ARM:
+            /* redefine forward direction */
+            value *= ARM_INVERT_DIR;
+
+            /*direction is backward: set DIR to 1 and value become positive*/
+            if(value<0){
                 gpio_set(ARM_PORT_DIR_1, ARM_PIN_DIR_1);
                 gpio_clear(ARM_PORT_DIR_2, ARM_PIN_DIR_2);
                 value *= -1;
-    		}
-    		/*direction is forward: set DIR to 0*/
-    		else{
+            }
+            /*direction is forward: set DIR to 0*/
+            else{
                 gpio_clear(ARM_PORT_DIR_1, ARM_PIN_DIR_1);
                 gpio_set(ARM_PORT_DIR_2, ARM_PIN_DIR_2);
-    		}
-    		/*calculate the up-time of EN between 0 and PWM_PERIOD*/
-    		speed = value * PWM_PERIOD/100;
-    		timer_set_oc_value(ACTUATOR_TIM, ARM_OC_ID, speed);
-    		break;
-    	case FLAG:
-    		/* redefine forward direction */
-    		value *= FLAG_INVERT_DIR;
-    	
-    		/*direction is backward: set DIR to 1 and value become positive*/
+            }
+            /*calculate the up-time of EN between 0 and PWM_PERIOD*/
+            speed = value * PWM_PERIOD/100;
+            timer_set_oc_value(ACTUATOR_TIM, ARM_OC_ID, speed);
+            break;
+        case FLAG:
+            /* redefine forward direction */
+            value *= FLAG_INVERT_DIR;
+
+            /*direction is backward: set DIR to 1 and value become positive*/
             if(value<0){
                 gpio_set(FLAG_PORT_DIR_1, FLAG_PIN_DIR_1);
                 gpio_clear(FLAG_PORT_DIR_2, FLAG_PIN_DIR_2);
                 value *= -1;
-    		}
-    		/*direction is forward: set DIR to 0*/
-    		else{
+            }
+            /*direction is forward: set DIR to 0*/
+            else{
                 gpio_clear(FLAG_PORT_DIR_1, FLAG_PIN_DIR_1);
                 gpio_set(FLAG_PORT_DIR_2, FLAG_PIN_DIR_2);
-    		}
-    		/*calculate the up-time of EN between 0 and PWM_PERIOD*/
-    		speed = value * PWM_PERIOD/100;
-    		timer_set_oc_value(ACTUATOR_TIM, FLAG_OC_ID, speed);
-    		break;
+            }
+            /*calculate the up-time of EN between 0 and PWM_PERIOD*/
+            speed = value * PWM_PERIOD/100;
+            timer_set_oc_value(ACTUATOR_TIM, FLAG_OC_ID, speed);
+            break;
     }
 }
