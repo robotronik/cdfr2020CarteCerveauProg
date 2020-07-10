c312d2b665602ad68ce7a3e23e2eb3d04919ab42
Victor <victor.millory@outlook.fr>
2020-07-10 12:01:31 +0200

    indentation

--- lowlevel/actuator.c
+++ lowlevel/actuator.c
@@ -8,20 +8,20 @@
 void actuator_setup(){
     /* Setup one timer for the flag and arm */
     timer_setup(ACTUATOR_TIM_RCC, ACTUATOR_TIM, PWM_PRESCALE, PWM_PERIOD);
-    
+
     /* setup arm EN*/
     gpio_setup_pin_af(ARM_GPIO_RCC_EN, ARM_PORT_EN, ARM_PIN_EN, ARM_AF);
-	timer_setup_output_c(ACTUATOR_TIM, ARM_OC_ID, ARM_OC_MODE, 0);
-    
+    timer_setup_output_c(ACTUATOR_TIM, ARM_OC_ID, ARM_OC_MODE, 0);
+
     /* setup arm DIR*/
     rcc_periph_clock_enable(ARM_GPIO_RCC_DIR_1);
-	gpio_mode_setup(ARM_PORT_DIR_1, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, ARM_PIN_DIR_1);
-	gpio_set_output_options(ARM_PORT_DIR_1, GPIO_OTYPE_PP,GPIO_OSPEED_50MHZ, ARM_PIN_DIR_1);
-	
+    gpio_mode_setup(ARM_PORT_DIR_1, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, ARM_PIN_DIR_1);
+    gpio_set_output_options(ARM_PORT_DIR_1, GPIO_OTYPE_PP,GPIO_OSPEED_50MHZ, ARM_PIN_DIR_1);
+
     rcc_periph_clock_enable(ARM_GPIO_RCC_DIR_21);
-	gpio_mode_setup(ARM_PORT_DIR_2, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, ARM_PIN_DIR_2);
-	gpio_set_output_options(ARM_PORT_DIR_2, GPIO_OTYPE_PP,GPIO_OSPEED_50MHZ, ARM_PIN_DIR_2);
-    
+    gpio_mode_setup(ARM_PORT_DIR_2, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, ARM_PIN_DIR_2);
+    gpio_set_output_options(ARM_PORT_DIR_2, GPIO_OTYPE_PP,GPIO_OSPEED_50MHZ, ARM_PIN_DIR_2);
+
     if(ARM_INIT_DIR){
         gpio_clear(ARM_PORT_DIR_1, ARM_PIN_DIR_1);
         gpio_set(ARM_PORT_DIR_2, ARM_PIN_DIR_2);
@@ -29,20 +29,20 @@ void actuator_setup(){
         gpio_clear(ARM_PORT_DIR_2, ARM_PIN_DIR_2);
         gpio_set(ARM_PORT_DIR_1, ARM_PIN_DIR_1);
     }
-    
+
     /* setup flag EN */
     gpio_setup_pin_af(FLAG_GPIO_RCC_EN, FLAG_PORT_EN, FLAG_PIN_EN, FLAG_AF);
-	timer_setup_output_c(ACTUATOR_TIM, FLAG_OC_ID, FLAG_OC_MODE, 0);
-    
+    timer_setup_output_c(ACTUATOR_TIM, FLAG_OC_ID, FLAG_OC_MODE, 0);
+
     /* setup flag DIR*/
     rcc_periph_clock_enable(FLAG_GPIO_RCC_DIR_1);
-	gpio_mode_setup(FLAG_PORT_DIR_1, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, FLAG_PIN_DIR_1);
-	gpio_set_output_options(FLAG_PORT_DIR_1, GPIO_OTYPE_PP,GPIO_OSPEED_50MHZ, FLAG_PIN_DIR_1);
-	
+    gpio_mode_setup(FLAG_PORT_DIR_1, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, FLAG_PIN_DIR_1);
+    gpio_set_output_options(FLAG_PORT_DIR_1, GPIO_OTYPE_PP,GPIO_OSPEED_50MHZ, FLAG_PIN_DIR_1);
+
     rcc_periph_clock_enable(FLAG_GPIO_RCC_DIR_21);
-	gpio_mode_setup(FLAG_PORT_DIR_2, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, FLAG_PIN_DIR_2);
-	gpio_set_output_options(FLAG_PORT_DIR_2, GPIO_OTYPE_PP,GPIO_OSPEED_50MHZ, FLAG_PIN_DIR_2);
-    
+    gpio_mode_setup(FLAG_PORT_DIR_2, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, FLAG_PIN_DIR_2);
+    gpio_set_output_options(FLAG_PORT_DIR_2, GPIO_OTYPE_PP,GPIO_OSPEED_50MHZ, FLAG_PIN_DIR_2);
+
     if(FLAG_INIT_DIR){
         gpio_clear(FLAG_PORT_DIR_1, FLAG_PIN_DIR_1);
         gpio_set(FLAG_PORT_DIR_2, FLAG_PIN_DIR_2);
@@ -50,9 +50,9 @@ void actuator_setup(){
         gpio_clear(FLAG_PORT_DIR_2, FLAG_PIN_DIR_2);
         gpio_set(FLAG_PORT_DIR_1, FLAG_PIN_DIR_1);
     }
-    
+
     /* start timer */
-	timer_start(ACTUATOR_TIM);
+    timer_start(ACTUATOR_TIM);
 }
 
 /**
@@ -63,49 +63,49 @@ void actuator_setup(){
  */
 void actuator_set(enum actuator_sel sel, int8_t value){
     /*value should be between -100 and +100*/
-	if(value < -100 || value > 100){
-		return;
-	}
+    if(value < -100 || value > 100){
+    	return;
+    }
 
-	int speed;
-	switch(sel){
-		case ARM:
-			/* redefine forward direction */
-			value *= ARM_INVERT_DIR;
-		
-			/*direction is backward: set DIR to 1 and value become positive*/
-			if(value<0){
-				gpio_set(ARM_PORT_DIR_1, ARM_PIN_DIR_1);
+    int speed;
+    switch(sel){
+    	case ARM:
+    		/* redefine forward direction */
+    		value *= ARM_INVERT_DIR;
+    	
+    		/*direction is backward: set DIR to 1 and value become positive*/
+    		if(value<0){
+    			gpio_set(ARM_PORT_DIR_1, ARM_PIN_DIR_1);
                 gpio_clear(ARM_PORT_DIR_2, ARM_PIN_DIR_2);
-				value *= -1;
-			}
-			/*direction is forward: set DIR to 0*/
-			else{
+    			value *= -1;
+    		}
+    		/*direction is forward: set DIR to 0*/
+    		else{
                 gpio_clear(ARM_PORT_DIR_1, ARM_PIN_DIR_1);
                 gpio_set(ARM_PORT_DIR_2, ARM_PIN_DIR_2);
-			}
-			/*calculate the up-time of EN between 0 and PWM_PERIOD*/
-			speed = value * PWM_PERIOD/100;
-			timer_set_oc_value(ACTUATOR_TIM, ARM_OC_ID, speed);
-			break;
-		case FLAG:
-			/* redefine forward direction */
-			value *= FLAG_INVERT_DIR;
-		
-			/*direction is backward: set DIR to 1 and value become positive*/
+    		}
+    		/*calculate the up-time of EN between 0 and PWM_PERIOD*/
+    		speed = value * PWM_PERIOD/100;
+    		timer_set_oc_value(ACTUATOR_TIM, ARM_OC_ID, speed);
+    		break;
+    	case FLAG:
+    		/* redefine forward direction */
+    		value *= FLAG_INVERT_DIR;
+    	
+    		/*direction is backward: set DIR to 1 and value become positive*/
             if(value<0){
-				gpio_set(FLAG_PORT_DIR_1, FLAG_PIN_DIR_1);
+    			gpio_set(FLAG_PORT_DIR_1, FLAG_PIN_DIR_1);
                 gpio_clear(FLAG_PORT_DIR_2, FLAG_PIN_DIR_2);
-				value *= -1;
-			}
-			/*direction is forward: set DIR to 0*/
-			else{
+    			value *= -1;
+    		}
+    		/*direction is forward: set DIR to 0*/
+    		else{
                 gpio_clear(FLAG_PORT_DIR_1, FLAG_PIN_DIR_1);
                 gpio_set(FLAG_PORT_DIR_2, FLAG_PIN_DIR_2);
-			}
-			/*calculate the up-time of EN between 0 and PWM_PERIOD*/
-			speed = value * PWM_PERIOD/100;
-			timer_set_oc_value(ACTUATOR_TIM, FLAG_OC_ID, speed);
-			break;
-	}
+    		}
+    		/*calculate the up-time of EN between 0 and PWM_PERIOD*/
+    		speed = value * PWM_PERIOD/100;
+    		timer_set_oc_value(ACTUATOR_TIM, FLAG_OC_ID, speed);
+    		break;
+    }
 }
